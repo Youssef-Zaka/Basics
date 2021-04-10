7229097368cde18373996a5b251c021a90602f2f
Zakadev <Zakaria.xperia.z2@gmail.com>
2021-04-10 17:07:35 +0200

    Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyFirstApp());
-}
+// void main() {
+//   runApp(MyFirstApp());
+// }
+
+void main() => runApp(MyFirstApp());
 
 // MyFirstAPP inherits StatelessWidget and is the main entry point widget
 // of the app
-class MyFirstApp extends StatelessWidget {
-  build(BuildContext context) {
+class MyFirstApp extends StatefulWidget {
+  @override
+  _MyFirstAppState createState() => _MyFirstAppState();
+}
+
+class _MyFirstAppState extends State<MyFirstApp> {
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+  }
+
+  var questionIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favourite color?',
+      'what\'s your favourite animal?',
+    ];
+
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () {
+                print('Not so easy man');
+              },
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () => print('so easy man'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
